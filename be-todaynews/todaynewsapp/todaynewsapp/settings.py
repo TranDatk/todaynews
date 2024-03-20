b33a8f1319913a7ab8990defb1f3de1708e83172
TranDatk <hgooshvhd123@gmail.com>
2024-03-20 16:29:31 +0700

    Update settings.py

--- be-todaynews/todaynewsapp/todaynewsapp/settings.py
+++ be-todaynews/todaynewsapp/todaynewsapp/settings.py
@@ -88,7 +88,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'todaynews',
         'USER': 'root',
-        'PASSWORD': '25052002',
+        'PASSWORD': '',
         'HOST': ''
     }
 }
